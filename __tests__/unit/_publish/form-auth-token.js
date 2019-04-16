36661f00f58f99b17622ce93535953534ff2c433
Alex Kilgour <alexjkilgour@gmail.com>
2019-04-16 13:37:25 +0100

    rewrite to fit package-manager style, add first test

---
+++ __tests__/unit/_publish/form-auth-token.js
@@ -0,0 +1,31 @@
+/**
+ * __tests__/unit/_publish/form-auth-token.js
+ * Test: js/_publish/_form-auth-token.js
+ */
+'use strict';
+
+const formAuthToken = require('../../../lib/js/_publish/_form-auth-token');
+
+describe('Generate Auth token URL', () => {
+	test('Handles http URL', () => {
+		const registryUrl = 'http://registry.npmjs.org/';
+		const result = formAuthToken(registryUrl, 'FOO');
+
+		expect.assertions(1);
+		expect(result).toEqual({
+			test: '//registry.npmjs.org/:_authToken=',
+			token: '//registry.npmjs.org/:_authToken=${FOO}'
+		});
+	});
+
+	test('Handles https URL', () => {
+		const registryUrl = 'https://registry.npmjs.org/';
+		const result = formAuthToken(registryUrl, 'FOO');
+
+		expect.assertions(1);
+		expect(result).toEqual({
+			test: '//registry.npmjs.org/:_authToken=',
+			token: '//registry.npmjs.org/:_authToken=${FOO}'
+		});
+	});
+});
