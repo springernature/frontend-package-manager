b60e3133c3b4dfffcff0c0ba88a39194a86a81b3
Alex Kilgour <alexjkilgour@gmail.com>
2019-10-17 11:01:52 +0100

    remove dependency on q - no longer needed

--- __mocks__/globby.js
+++ __mocks__/globby.js
@@ -4,27 +4,24 @@
  */
 'use strict';
 
-const q = require('q');
 const results = require('./glob-results');
 
 const resultsToolkits = results.toolkitFiles();
 const resultsPackage = results.packageFiles();
 
 const globby = (path, _options) => {
-	const deferred = q.defer();
-
-	// Check toolkit results
-	if (path.includes('toolkits')) {
-		if (path.includes('toolkits-no-globby')) {
-			deferred.reject(new Error('globby error'));
+	return new Promise((resolve, reject) => {
+		// Check toolkit results
+		if (path.includes('toolkits')) {
+			if (path.includes('toolkits-no-globby')) {
+				reject(new Error('globby error'));
+			}
+			resolve(resultsToolkits[path]);
 		}
-		deferred.resolve(resultsToolkits[path]);
-		return deferred.promise;
-	}
 
-	// Check package results
-	deferred.resolve(resultsPackage[path]);
-	return deferred.promise;
+		// Check package results
+		resolve(resultsPackage[path]);
+	});
 };
 
 module.exports = globby;
