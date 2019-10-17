30b1ea34f5c6d00f8c7599c0813010cbefb06167
Alex Kilgour <alexjkilgour@gmail.com>
2019-10-17 15:34:47 +0100

    async check-exists

--- __tests__/unit/_utils/check-exists.js
+++ __tests__/unit/_utils/check-exists.js
@@ -14,44 +14,44 @@ describe('Check if files/folders exist on the filesystem', () => {
 		mockfs(MOCK_PACKAGES);
 	});
 
-	test('Resolve if file exists', () => {
+	test('Resolve if file exists', async () => {
 		expect.assertions(1);
-		return expect(
+		await expect(
 			exists.fileExists('path/to/some.png')
 		).resolves.toEqual('path/to/some.png');
 	});
 
-	test('Resolve if folder exists', () => {
+	test('Resolve if folder exists', async () => {
 		expect.assertions(1);
-		return expect(
+		await expect(
 			exists.directoryExists('some/other/path')
 		).resolves.toEqual('some/other/path');
 	});
 
-	test('Reject if file does NOT exist', () => {
+	test('Reject if file does NOT exist', async () => {
 		expect.assertions(1);
-		return expect(
+		await expect(
 			exists.fileExists('path/to/other.png')
 		).rejects.toBeInstanceOf(Error);
 	});
 
-	test('Reject if folder does NOT exist', () => {
+	test('Reject if folder does NOT exist', async () => {
 		expect.assertions(1);
-		return expect(
+		await expect(
 			exists.directoryExists('some/other/invalid/path')
 		).rejects.toBeInstanceOf(Error);
 	});
 
-	test('Reject if file when checking directory', () => {
+	test('Reject if file when checking directory', async () => {
 		expect.assertions(1);
-		return expect(
+		await expect(
 			exists.directoryExists('path/to/other.png')
 		).rejects.toBeInstanceOf(Error);
 	});
 
-	test('Reject if directory when checking file', () => {
+	test('Reject if directory when checking file', async () => {
 		expect.assertions(1);
-		return expect(
+		await expect(
 			exists.fileExists('some/other/path')
 		).rejects.toBeInstanceOf(Error);
 	});
